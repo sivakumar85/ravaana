333915597a912776078684935e4bc9481ec54c42
Teja <teja85131@gmail.com>
2018-12-26 17:13:34 +0530

    changes

--- php/Instamojo/config.php
+++ php/Instamojo/config.php
@@ -3,9 +3,9 @@
     $api_key = 'test_1ce598eb22a3d011ae6b129facd';
     $api_secret = 'test_b8fe581be127b6b22f245afca52';
     $api_salt = 'b56ee3d97942477eb4cd5d0091d22fe0';
-	$webhook_url = 'http://ravaana.000webhostapp.com/Instamojo/webhook.php';
+	$webhook_url = 'http://ravaana.000webhostapp.com/php/Instamojo/webhook.php';
 	//$redirect_url = 'http://ravaana.000webhostapp.com/Instamojo/thanks.php';
-    $redirect_url = 'http://localhost:1080/ravaana/php/Instamojo/thanks.php';
+    $redirect_url = 'http://localhost/ravaana/php/Instamojo/thanks.php';
     
     $mode = "test"; //You can change it to live by jest replacing it by 'live'
     if($mode == 'live'){
