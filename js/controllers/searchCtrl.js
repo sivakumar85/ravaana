b111a966b4dda5c91bb41243bfac068131f69694
Teja <teja85131@gmail.com>
2019-01-03 10:57:21 +0530

    changes

--- js/controllers/searchCtrl.js
+++ js/controllers/searchCtrl.js
@@ -139,6 +139,51 @@ app.controller('searchCtrl', ['$scope','$rootScope','$http','$location','$routeP
 				 $location.path('/SignIn/loginMsg');
 		}
 	};
+	$scope.sort = {       
+					sortingOrder : 'typeOfLoad',
+					reverse : false
+				};
 
 }]);
+app.directive("userloadSort", function() {
+		return {
+			restrict: 'A',
+			transclude: true,    
+			scope: {
+			  order: '=',
+			  sort: '='
+			},
+			template : 
+			  ' <a href="javascript:void(0);" ng-click="sort_by(order)" style="color: #555555;">'+
+			  '    <span ng-transclude></span>'+
+			  '    <i ng-class="selectedCls(order)"></i>'+
+			  '</a>',
+			link: function(scope) {
+						
+			// change sorting order
+			scope.sort_by = function(newSortingOrder) {       
+				var sort = scope.sort;
+				
+				if (sort.sortingOrder == newSortingOrder){
+					sort.reverse = !sort.reverse;
+				}                    
+
+				sort.sortingOrder = newSortingOrder;        
+			};
+			
+		   
+			scope.selectedCls = function(column) {
+				if(column == scope.sort.sortingOrder){
+					return ('fa fa-sort-' + ((scope.sort.reverse) ? 'desc' : 'asc'));
+				}
+				else{            
+					return'fa fa-sort' 
+				} 
+			};      
+		  }
+		}
+
+	});
+
+
 
